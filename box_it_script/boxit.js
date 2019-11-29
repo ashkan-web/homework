91500ed802967686bf0ff0945b0d2cb37dc0d9ab
ashkan <ashkanh.info@gmail.com>
2019-11-28 21:23:04 -0800

    impelementing 3 function for drawing

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -1,9 +1,18 @@
-let str="━"
-let number= parseInt(process.argv[2]);
-function drawLine(number){
+let str="━";
+// function that takes a number as an argument that returns that number of horizontal bars
+ function drawLine(number){
 return str.repeat(number)
 }
-console.log(drawLine(number));
+//drawTopBorder, drawMiddleBorder and drawBottomBorder Functions
+//takes a number as an argument that returns that number of horizontal bars with starting character and end character
+function drawTopBorder(number){
+    return "┏" + drawLine(number) + "┓"
+    }
 
-function 
-
+    function drawMiddleBorder(number){
+        return "┣" + drawLine(number) + "┫"
+        }
+        
+        function drawBottomBorder(number){
+            return "┗" + drawLine(number) + "┛"
+            }
