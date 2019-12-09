a40eca492635052f6bf196c39d100d25def9389c
ashkan <ashkanh.info@gmail.com>
2019-12-08 23:09:36 -0800

    function forward and rigth turn has been added

--- turtle_graphics/turtle_graphics.js
+++ turtle_graphics/turtle_graphics.js
@@ -2,15 +2,46 @@
 const arr=[];
 class turtle{
     constructor(x, y) {
+        this.step=[[0,0]]
         this.x = x;
         this.y = y;
+        this.move=East
     }
    
-
+//forward method that takes a number of steps then updates the Turtle instance with its new position after moving that many steps
+   
     forward(n) {
         this.x = this.x+n;
         this.y = this.y;
+
+    switch(this.move){
+
+     case East:
+        this.step.push([x+n,y]);
+        break;
+     case West:
+        this.step.push([x-n,y]);
+        break;   
+     case South:
+        this.step.push([x,y+n]);
+        break;
+     case North:
+        this.step.push([x,y-n]);
+        break;   
     }
+
+
+    return this;
+
+    }
+
+//rigth method that takes zero arguments. When right is called, update the Turtle instance to rotate its facing to the right
+
+rigth(){
+    this.move = (this.mov)
+}
+
+
 }
 
     
